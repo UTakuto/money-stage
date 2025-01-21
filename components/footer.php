5252718c841aa3606a2f92ef62ff12675c079488
UTakuto <taku.0307.ue@icloud.com>
2025-01-21 13:49:49 +0900

    footerのスタイルを修正し、背景色を追加

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,4 @@
-<footer class="text-center border-solid border-t-[1px] border-[#333] p-4">
+<footer class="w-full text-center border-solid border-t-[1px] border-[#333] bg-[#fffff5] p-4">
     <p>
         <small>2024 &copy; money-stage</small>
     </p>
