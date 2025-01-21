efbaedc68ee061fcc96c47123b8481585bd020c4
UTakuto <taku.0307.ue@icloud.com>
2025-01-21 19:36:32 +0900

    データベースのmoney_resultsテーブルのカラム型を変更し、store.phpにデータ送信処理とバリデーションを追加

--- database/result.sql
+++ database/result.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS money_results;
 CREATE TABLE money_results (
     id          SERIAL PRIMARY KEY,                            # 主キー
     name        VARCHAR(255) NOT NULL,                         # 名前
-    age         VARCHAR(255) NOT NULL,                         # 年齢
-    income      VARCHAR(255) NOT NULL,                         # 収入
+    age         TINYINT UNSIGNED NOT NULL,                     # 年齢
+    income      INT UNSIGNED NOT NULL,                         # 収入
     marriage    TINYINT UNSIGNED DEFAULT 1,                    # 結婚(1:結婚, 2:未婚)
     children    TINYINT UNSIGNED DEFAULT 1,                    # 子供(1:有, 2:無)
     housing     TINYINT UNSIGNED DEFAULT 1,                    # 住宅(1:賃貸, 2:持ち家, 3:社宅・寮, 4:その他)
--- store.php
+++ store.php
@@ -1,4 +1,78 @@
 <?php
+// store.php
 
-var_dump($_POST);
+require "./config.php";
+// var_dump($_POST);
 
+
+
+try{
+    //送信データの取得
+    $name     = filter_input(INPUT_POST, "name");
+    $age      = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
+    $income   = filter_input(INPUT_POST, "income", FILTER_VALIDATE_INT);
+    $marriage = filter_input(INPUT_POST, "marriage", FILTER_VALIDATE_INT);
+    $children = filter_input(INPUT_POST, "children", FILTER_VALIDATE_INT);
+    $housing  = filter_input(INPUT_POST, "housing", FILTER_VALIDATE_INT);
+
+    // 送信データの確認
+    var_dump($name , $age , $income , $marriage , $children , $housing);
+
+    // 未入力チェック
+    if(!$name){
+        throw new Exception("名前が未入力です");
+    }
+    if(!$age){
+        throw new Exception("年齢が未入力です");
+    }
+    if(!$income){
+        throw new Exception("年収が未入力です");
+    }
+    if(!$marriage){
+        throw new Exception("婚姻の有無が未入力です");
+    }
+    if(!$children){
+        throw new Exception("こどもの有無が未入力です");
+    }
+    if(!$housing){
+        throw new Exception("住まいのタイプが未入力です");
+    }
+
+    // DB接続
+    $db = new PDO(DB_DSN, DB_USER, DB_PASS );
+
+    // トランザクション開始
+    $db->beginTransaction();
+
+    $resultTable = TB_RESULT;
+
+    $resultSql ="
+        INSERT INTO {$resultTable} (name, age, income, marriage, children, housing)
+        VALUES (:name, :age, :income, :marriage, :children, :housing)
+    ";
+
+    $stmt = $db->prepare($resultSql);
+    $stmt -> execute(
+        [
+            "name"     => $name,
+            "age"      => $age,
+            "income"   => $income,
+            "marriage" => $marriage,
+            "children" => $children,
+            "housing"  => $housing
+        ]
+    );
+
+    // コミット
+    $db->commit();
+}
+catch(PDOException $error){
+    print $error->getMessage() . ">>>" . $stmt->queryString;
+    //ロールバック
+    $db->rollBack();
+}
+catch(Exception $error){
+    print $error->getMessage();
+     //ロールバック
+    $db->rollBack();
+}
