2b8cd8a18dc39a2608bccf09264510552aa3c03a
UTakuto <taku.0307.ue@icloud.com>
2025-01-21 18:27:32 +0900

    components設定 , 修正

---
+++ components/head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>money-stage</title>
+    <!-- tailwindcss -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
--- components/header.php
+++ components/header.php
@@ -2,11 +2,11 @@
 
 ?>
 <header class="w-full h-[10vh]">
-    <nav class="h-[8vh] bg-[#094e80] text-[#fefefe]">
+    <nav class="h-[10vh] bg-[#094e80] text-[#fefefe]">
         <div class="h-full flex justify-start items-center pl-[20px] ">
             <div>
                 <a href="#" class="flex justify-center items-center gap-[10px] w-full h-[8vh] text-lg font-bold">
-                    <img class="w-[50px] h-fit" src="./img/logo.png" alt="">
+                    <img class="w-[100px] h-fit" src="./img/logo1.png" alt="マネステージロゴ">
                     <img class="w-[200px] h-fit pt-[8px] " src="./img/title_white.png" alt="マネステージ">
                 </a>
             </div>
