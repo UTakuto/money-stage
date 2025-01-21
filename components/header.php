c7878e27549b22a7a318094a45eb0ad3a1af3ee6
UTakuto <taku.0307.ue@icloud.com>
2025-01-21 11:40:19 +0900

    headerのコンポーネント化

---
+++ components/header.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<header class="w-full h-[10vh]">
+    <nav class="h-[8vh] bg-[#072942] text-[#fefefe]">
+        <div class="h-full flex justify-start items-center pl-[20px] ">
+            <div>
+                <a href="#" class="flex justify-center items-center gap-[10px] w-full h-[8vh] text-lg font-bold">
+                    <img class="w-[50px] h-fit" src="./img/logo.png" alt="">
+                    <img class="w-[200px] h-fit pt-[8px] " src="./img/title_white.png" alt="マネステージ">
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>
