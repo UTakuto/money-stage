aa9d2fd347c49ad73d5f894aef9f582a920ff54a
UTakuto <taku.0307.ue@icloud.com>
2025-01-22 09:49:16 +0900

    婚姻に関するラベルを「ある」と「ない」から「している」と「していない」に変更

--- index.php
+++ index.php
@@ -32,13 +32,13 @@
                     <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                         <div class="flex items-center ps-3">
                             <input id="yes-marriage-radio" value="1" type="radio" name="marriage" value="yes" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2">
-                            <label for="yes-marriage-radio" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ある</label>
+                            <label for="yes-marriage-radio" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">している</label>
                         </div>
                     </li>
                     <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                         <div class="flex items-center ps-3">
                             <input id="no-marriage-radio" value="2" type="radio" name="marriage" value="no" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2">
-                            <label for="no-marriage-radio" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ない</label>
+                            <label for="no-marriage-radio" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">していない</label>
                         </div>
                     </li>
                 </ul>
