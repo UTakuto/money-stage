09a2da3b6b698dd0dc8f2058f38e3f0af01b8c1e
UTakuto <taku.0307.ue@icloud.com>
2025-02-07 01:35:02 +0900

    ファビコンとAppleタッチアイコンを追加

--- components/head.php
+++ components/head.php
@@ -2,6 +2,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>money-stage</title>
+    <link rel="icon" type="image/x-icon" href="../img/logo.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../img/logo.png">
     <!-- tailwindcss -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
