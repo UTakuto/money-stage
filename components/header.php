42bdc45000af323d3b7378c5ca37f4d553846fa2
UTakuto <taku.0307.ue@icloud.com>
2025-02-07 01:35:30 +0900

    ヘッダーのリンクを更新し、ロゴとタイトル画像のスタイルを改善

--- components/header.php
+++ components/header.php
@@ -3,11 +3,11 @@
 ?>
 <header class="w-full h-[10vh]">
     <nav class="h-[10vh] bg-[#062940] text-[#fefefe]">
-        <div class="h-full flex justify-start items-center pl-[20px] ">
+        <div class="h-full flex justify-start items-center pl-[20px]">
             <div>
-                <a href="../index.php" class="flex justify-center items-center gap-[10px] w-full h-[8vh] text-lg font-bold">
-                    <img class="w-[80px] h-fit" src="./img/logo_white.png" alt="マネステージロゴ">
-                    <img class="w-[200px] h-fit pt-[8px] " src="./img/title_white.png" alt="マネステージ">
+                <a href="https://click.ecc.ac.jp/ecc/tuemori/money-stage/index.php" class="flex justify-center items-center gap-[10px] w-full h-[8vh] text-lg font-bold">
+                    <img class="min-w-[80px] max-w-[80px] h-auto" src="./img/logo_white.png" alt="マネステージロゴ">
+                    <img class="min-w-[200px] max-w-[200px] h-auto pt-[8px]" src="./img/title_white.png" alt="マネステージ">
                 </a>
             </div>
         </div>
